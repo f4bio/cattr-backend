b570e1f1c7e7f0ea2aeb8659bfd21e9e71376c17
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-02-19 17:02:57 +0600

    removed Action determinator

--- app/Jobs/ExportReportInGoogleSheetsJob.php
+++ app/Jobs/ExportReportInGoogleSheetsJob.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\RequestOptions;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -35,20 +34,21 @@ class ExportReportInGoogleSheetsJob implements ShouldQueue
         $logger->debug(sprintf("Job %s started. State: %s", self::class, json_encode($this->state)));
 
         try {
-            $httpClient->request(
-                'POST',
-                sprintf(
-                    "%s/api/v1/google-sheet-report",
-                    config('app.google_integration_bus.url'),
-                ),
-                [
-                    RequestOptions::JSON => [
-                        'state' => base64_encode(json_encode($this->state, JSON_THROW_ON_ERROR)),
-                        'userId' => $this->state['userId'],
-                        'instanceId' => $this->state['instanceId'],
-                    ]
-                ]
-            );
+
+//            $httpClient->request(
+//                'POST',
+//                sprintf(
+//                    "%s/api/v1/google-sheet-report",
+//                    config('app.google_integration_bus.url'),
+//                ),
+//                [
+//                    RequestOptions::JSON => [
+//                        'state' => base64_encode(json_encode($this->state, JSON_THROW_ON_ERROR)),
+//                        'userId' => $this->state['userId'],
+//                        'instanceId' => $this->state['instanceId'],
+//                    ]
+//                ]
+//            );
             // get report: extract & build
             // export via api
             // send notification OK
