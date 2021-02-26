3e8b4dcaabe169e5275b5d0ea81c2d699db88e57
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-02-26 18:40:23 +0600

    added views export_end_fail, export_end_success

---
+++ resources/views/google/sheets/export_end_fail.blade.php
@@ -0,0 +1,3 @@
+<p style="text-align: center; font-size: large">
+    Sorry, but something goes bad...
+</p>
---
+++ resources/views/google/sheets/export_end_success.blade.php
@@ -0,0 +1,3 @@
+<p style="text-align: center; font-size: large">
+    Ok, we have accepted your request, we will send this report to your email!
+</p>
